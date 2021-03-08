7a503a311cd29a80fed400c33c44445e0e26d744
Rodolphe <dutchr@live.fr>
2021-03-08 16:39:52 +0100

    adding avatar w/ small styles

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Avatar from './components/Avatar'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  <Avatar initials= "RD" size={35} backgroundColor={'green'} 
+  
+  />
+  )
 }
 
 const styles = StyleSheet.create({
